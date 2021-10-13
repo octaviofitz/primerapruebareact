945c38e64289bfb3948a9610694b1e25e5ead6e4
octaviofitz <octaviofitz@gmail.com>
2021-10-13 15:40:31 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/master.css';
+import Web from './components/Web';
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
